9308b5687461e57e7387a1f572873fe3135cfd0a
zhangyuyao <zhangyuyao@live.cn>
2018-10-10 08:16:03 +0800

    使用Spring Boot创建应用

--- src/main/java/contacts/ContactController.java
+++ src/main/java/contacts/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -16,14 +17,39 @@ public class ContactController {
     @Autowired
     private ContactRepository contactRepository;
 
+    /**
+     * 获取所有联系人
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Map<String, Object> model) {
+        List<Contact> contacts = contactRepository.findAll();
+        model.put("contacts", contacts);
+        return "home";
+    }
+
+    /**
+     * 保存联系人
+     *
+     * @param contact
+     * @return
+     */
     @RequestMapping(method = RequestMethod.POST)
+    public String submit(Contact contact) {
+        contactRepository.save(contact);
+        return "redirect:/";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/save")
     @ResponseBody
     public int save(@RequestBody Contact contact) {
         contactRepository.save(contact);
         return 1;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, value = "/findAll")
     @ResponseBody
     public List<Contact> findAll() {
         return contactRepository.findAll();
